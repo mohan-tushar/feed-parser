d96f1d6ac2f0cd547eb31f6ece4b6dc74a2412d7
Tushar Mohan <mohan.tushar>
2019-07-01 01:25:33 +0530

    Updates

---
+++ precache-manifest.ba4dd700b3707f79d0aad19f76bff72b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e9fd1001a6c109bd94f27e8a6ee70e7",
+    "url": "/feed-parser/index.html"
+  },
+  {
+    "revision": "22d14bdd1def92d31450",
+    "url": "/feed-parser/static/js/2.41597cef.chunk.js"
+  },
+  {
+    "revision": "5993369399b00ea0e449",
+    "url": "/feed-parser/static/js/main.b23e459e.chunk.js"
+  },
+  {
+    "revision": "ba850e298aca51a4c5df",
+    "url": "/feed-parser/static/js/runtime~main.b8cfb3aa.js"
+  }
+]);
