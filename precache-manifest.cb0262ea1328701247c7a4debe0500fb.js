eda5e2c189c2e89b07c2245c15f659c7e14a038c
Tushar Mohan <mohan.tushar>
2019-07-01 00:58:22 +0530

    Initial Commit

---
+++ precache-manifest.cb0262ea1328701247c7a4debe0500fb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dedafaea7dc000267379d1bd5d913ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22d14bdd1def92d31450",
+    "url": "/static/js/2.41597cef.chunk.js"
+  },
+  {
+    "revision": "648b08ecd870d5073a1b",
+    "url": "/static/js/main.2cfed3c2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
